6d6dfc436fe1c79bc5b7b7e6a5f049a36d17146d
Brendon Maia Lopes <brendonmaial@poli.ufrj.br>
2025-08-07 14:43:12 -0300

    Concurrent movement in x and y with varying speeds ( x and y reach destination at the same time )

--- motor.ino
+++ motor.ino
@@ -1,9 +1,13 @@
 #include <Stepper.h>
+
 const int stepsPerRev = 200;
 const int rpm          = 15;
 const int height = 500;
 const int threshold = 1;
+const int k_threshold = 5;
 
+const int quickStep = 4;
+const int slowStep = 1;
 Stepper stepperX(stepsPerRev, 8, 9, 10, 11);
 Stepper stepperY(stepsPerRev, 2, 3, 4, 5);
 Stepper stepperZ(stepsPerRev, 6, 7, 12, 13);
@@ -20,7 +24,86 @@ void setup() {
   Serial.begin(9600);
 }
 
-void moveTo(int xTarget , int yTarget, int zTarget) {
+void moveTo(int xTarget, int yTarget){
+  const int a = abs(xTarget-xPos);
+  const int b = abs(yTarget-yPos); 
+  float prop = (float)min(a,b)/(float)max(a,b);
+//Find the irreductible fraction for prop and use denom in x or y and numerator in y or x
+  while(abs(xPos-xTarget)>=threshold | abs(yPos-yTarget)>=threshold){
+    if(xPos-xTarget>0){
+      if(abs(xPos-xTarget)<k_threshold*threshold){
+        if(round(prop*slowStep)==0){
+          stepperX.step(-1);
+          xPos = xPos - 1;
+        }else{
+          stepperX.step(-round(prop*slowStep));
+          xPos = xPos - round(prop*slowStep);
+        }
+        Serial.println("Slow step on -x");
+        // Serial.println(round(prop*slowStep));
+      }else{
+        stepperX.step(-round(prop*quickStep));
+        xPos = xPos - round(prop*quickStep);
+        Serial.println("Quick step on -x");
+        // Serial.println(prop*quickStep);
+      }
+    }
+    if(xPos-xTarget<0){
+      if(abs(xPos-xTarget)<k_threshold*threshold){
+        if(round(prop*slowStep)==0){
+          stepperX.step(1);
+          xPos = xPos + 1;
+        }else{
+          stepperX.step(round(prop*slowStep));
+          xPos = xPos + round(prop*slowStep);
+        }
+        Serial.println("Slow step on +x");
+        // Serial.println(prop*slowStep);
+      }else{
+        stepperX.step(round(prop*quickStep));
+        xPos = xPos + round(prop*quickStep);
+        Serial.println("Quick step on +x");
+        // Serial.println(round(prop*quickStep));
+      }
+    }
+    if(xPos - xTarget == 0 ){
+      Serial.println("x on target");
+    }
+  
+    if(yPos-yTarget>0){
+      if(abs(yPos-yTarget)<k_threshold*threshold){
+        stepperY.step(-slowStep);
+        yPos = yPos - slowStep;
+        // Serial.println("Slow step on y");
+        // Serial.println(prop*slowStep);
+      }else{
+        stepperY.step(-quickStep);
+        yPos = yPos - quickStep;
+        // Serial.println("Quick step on y");
+        // Serial.println(prop*quickStep);
+      }
+    }
+    if(yPos-yTarget<0){
+      if(abs(yPos-yTarget)<k_threshold*threshold){
+        stepperY.step(slowStep);
+        yPos = yPos + slowStep;
+        // Serial.println("Slow step on y");
+        // Serial.println(prop*slowStep);
+      }else{
+        stepperY.step(quickStep);
+        yPos = yPos + quickStep;
+        // Serial.println("Quick step on y");
+        // Serial.println(prop*quickStep);
+      }
+    }
+    if(yPos - yTarget == 0 ){
+      // Serial.println("y on target");
+    }
+    // Serial.println("______________");
+  }
+}
+
+void findPos(int xTarget , int yTarget, int zTarget) {
   Serial.println("z Rectract");
   for(int i = 0 ; i < height ; i++){
     if(zPos<=0){
@@ -72,4 +155,20 @@ void moveTo(int xTarget , int yTarget, int zTarget) {
   delay(1000);
 }
 
-void loop(){}
+void loop(){
+  String cmd = Serial.readStringUntil('\n');
+  if(cmd=="home"){
+    moveTo(100,100);
+  }
+  if(cmd=="zero"){
+    moveTo(0,0);
+  }
+  if(cmd=="random"){
+    const int x = (int) random(200);
+    const int y = (int) random(200);
+    Serial.println(x);
+    Serial.println(y);
+    Serial.println("_____");
+    moveTo(x,y);
+  }
+}
