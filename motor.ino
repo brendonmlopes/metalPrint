672ee19e5ac4d135c18dba919f5090d624b3aa50
Brendon Maia Lopes <brendonmaial@poli.ufrj.br>
2025-08-08 16:42:11 -0300

    Speed control with dt(delay) added

--- motor.ino
+++ motor.ino
@@ -1,7 +1,8 @@
 #include <Stepper.h>
+#include <string.h>
 
 const int stepsPerRev = 200;
-const int rpm          =15;
+const int rpm          =60;
 const float maxX = 1000.;
 const float maxY = 1000.;
 const int maxZ = 500;
@@ -21,9 +22,13 @@ void setup() {
   stepperY.setSpeed(rpm);
   stepperZ.setSpeed(rpm);
   Serial.begin(9600);
+
+  moveTo(100,200,0);
+  moveTo(0,0,0);
+  moveTo(100,200,5);
 }
 
-void moveTo(int xTarget, int yTarget){
+void moveTo(int xTarget, int yTarget, float dt){
   const int distX = abs(xTarget-xPos);
   const int distY = abs(yTarget-yPos);
   double bufferX = 0; 
@@ -32,15 +37,19 @@ void moveTo(int xTarget, int yTarget){
   while(abs(xPos-xTarget)>=threshold | abs(yPos-yTarget)>=threshold){
     bufferX+= distX/maxX ;
     bufferY+= distY/maxY ;
+    delay(dt);
+
     if(bufferX>=1){
       bufferX-=1;
       if(xPos-xTarget>0){
         stepperX.step(-1);
         xPos = xPos - 1;
+        // Serial.println("Step -x");
       }
       if(xPos-xTarget<0){
         stepperX.step(1);
         xPos = xPos + 1;
+        // Serial.println("Step +x");
       }
     }
     
@@ -62,6 +71,7 @@ void moveTo(int xTarget, int yTarget){
     if(yPos - yTarget == 0 ){
       Serial.println("y on target");
     }
+    // Serial.println("______________");
   }
   Serial.println("xTarget");
   Serial.println(xTarget);
@@ -92,10 +102,6 @@ void findPos(int xTarget , int yTarget, int zTarget) {
     if(xPos - xTarget == 0 ){
       Serial.println("x on target");
     }
-    Serial.println("____________");
-    Serial.println("x");
-    Serial.println(xPos);
-    Serial.println(xPos-xTarget);
   
     if(yPos-yTarget>0){
       stepperY.step(-1);
@@ -108,10 +114,6 @@ void findPos(int xTarget , int yTarget, int zTarget) {
     if(yPos - yTarget == 0 ){
       Serial.println("y on target");
     }
-    Serial.println("____________");
-    Serial.println(("y"));
-    Serial.println(yPos);
-    Serial.println(yPos-yTarget);
   }
   while(abs(zPos-zTarget)>=threshold){
     stepperZ.step(1);
@@ -125,15 +127,15 @@ void findPos(int xTarget , int yTarget, int zTarget) {
 void loop(){
   String cmd = Serial.readStringUntil('\n');
   if(cmd=="home"){
-    moveTo(103,114);
+    moveTo(103,114,0);
   }
   if(cmd=="zero"){
-    moveTo(0,0);
+    moveTo(0,0,0);
   }
   if(cmd=="random"){
     const int x = (int) random(200);
     const int y = (int) random(200);
-    moveTo(x,y);
+    moveTo(x,y,0);
     delay(1000);
   }
 }
