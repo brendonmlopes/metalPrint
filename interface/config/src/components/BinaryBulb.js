749628ca71b78f04845b9bfef35c0b265ed79fbb
Brendon Maia Lopes <brendonmaial@poli.ufrj.br>
2025-08-21 18:17:36 -0300

    Added sensor viewers to the settings page

--- interface/config/src/components/BinaryBulb.js
+++ interface/config/src/components/BinaryBulb.js
@@ -1,17 +1,19 @@
 import LightbulbIcon from '@mui/icons-material/Lightbulb';
+import LightbulbOutlineIcon from '@mui/icons-material/LightbulbOutline';
 import Box from '@mui/material/Box';
 import {useState} from 'react';
 
 export default function BinaryBulb(props){
   let title = props.title
-  let on = props.on
-  let [state, setState] = useState(true)
+  let on = !!props.on
+  let [state, setState] = useState(!!on)
+  let component = on ? <LightbulbIcon color={"warning"} /> : <LightbulbOutlineIcon color={"warning"}/>
   return(
     <>
       <Box sx={{backgroundColor:'#999999', borderRadius:1}}>
         <div className="mt-2">
           <span className="mt-1 h4">{title}</span>
-          <LightbulbIcon color={"warning"}/>
+            {component}
         </div>
       </Box>
     </>
--- interface/config/src/pages/Settings.js
+++ interface/config/src/pages/Settings.js
@@ -34,25 +34,51 @@ export default function Settings(props) {
     energySourceConnect:false,
   }) 
 
+
+
   return(
     <header className="App-header">
 
-      <div className="container">
+      <div className="container container-fluid">
         <div className="row">
           <div className="col-2">
-            <ToggleController title="All motors" control={[{title:'Z 1'},{title:'Z 2'},{title:'Z 3'},{title:'Z 4'},{title:'Y 1'},{title:'Y 2'},{title:'X'}]} fontSize={32} />
+            <h2>Power motors</h2>
           </div>
 
+            <div className="col-7">
+            </div>
+
+            <div className="col-3">
+              <h2>Connections</h2>
+            </div>
+        </div>
+        <div className="row">
           <div className="col-2">
+            <ToggleController title="All motors" control={[{title:'X'},{title:'Y 1'},{title:'Y 2'},{title:'Z 1'},{title:'Z 2'},{title:'Z 3'},{title:'Z 4'},]} fontSize={32} />
           </div>
-          <div className="col-2 p-1">
-            <BinaryBulb title={"Motor X 1 Connection"} />
-            <BinaryBulb title={"Motor X 2 Connection"} />
-            <BinaryBulb title={"Motor Y Connection"} />
-            <BinaryBulb title={"Motor Z 1 Connection"} />
-            <BinaryBulb title={"Motor Z 2 Connection"} />
-            <BinaryBulb title={"Motor Z 3 Connection"} />
-            <BinaryBulb title={"Motor Z 4 Connection"} />
+
+          <div className="col-7">
+          </div>
+          <div className="col-1 p-1">
+            <BinaryBulb on={state['motorX1Connect']} title={"M X 1"} />
+            <BinaryBulb on={state['motorX2Connect']} title={"M X 2"} />
+            <BinaryBulb on={state['motorY1Connect']} title={"M Y 1"} />
+            <BinaryBulb on={state['motorZ1Connect']} title={"M Z 1"} />
+            <BinaryBulb on={state['motorZ2Connect']} title={"M Z 2"} />
+            <BinaryBulb on={state['motorZ3Connect']} title={"M Z 3"} />
+            <BinaryBulb on={state['motorZ4Connect']} title={"M Z 4"} />
+          </div>
+          <div className="col-1 p-1">
+            <BinaryBulb on={state['sensorX1Connect']} title={"S X 1"} />
+            <BinaryBulb on={state['sensorX2Connect']} title={"S X 2"} />
+            <BinaryBulb on={state['sensorY1Connect']} title={"S Y 1"} />
+            <BinaryBulb on={state['sensorZ1Connect']} title={"S Z 1"} />
+            <BinaryBulb on={state['sensorZ2Connect']} title={"S Z 2"} />
+            <BinaryBulb on={state['sensorZ3Connect']} title={"S Z 3"} />
+            <BinaryBulb on={state['sensorZ4Connect']} title={"S Z 4"} />
+          </div>
+          <div className="col-1 p-1">
+            <BinaryBulb on={false} title={"Power"} />
           </div>
         </div>
       </div>
