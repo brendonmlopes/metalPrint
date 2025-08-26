7712e490caa8c3154d73e24cf6961f5f321ac256
Brendon Maia Lopes <brendonmaial@poli.ufrj.br>
2025-08-26 14:12:43 -0300

    Add + and - to sensor components

--- interface/config/src/components/BinaryBulb.js
+++ interface/config/src/components/BinaryBulb.js
@@ -5,15 +5,55 @@ import {useState} from 'react';
 
 export default function BinaryBulb(props){
   let title = props.title
-  let on = !!props.on
-  let [state, setState] = useState(!!on)
-  let component = on ? <LightbulbIcon color={"warning"} /> : <LightbulbOutlineIcon color={"warning"}/>
+  let data = props.data
+  let [state, setState] = useState({plus:false,minus:false})
+  let color;
+  let colorPlus;
+  let colorMinus;
+  let component;
+
+  //For motor sensors or stopper sensors
+  if(title.startsWith("M") || title.startsWith("S")){
+    if (data==0) {
+      color = "#808688"
+      colorPlus = "#808688"
+      colorMinus = "#808688"
+    } else if(data==1){
+      color = "#38ad14"
+      colorPlus = "#808688"
+      colorMinus = "#808688"
+    } else if(data==2){
+      color = "#38ad14"
+      colorPlus = "#38ad14"
+      colorMinus = "#808688"
+    } else if(data==3){
+      color = "#38ad14"
+      colorPlus = "#808688"
+      colorMinus = "#38ad14"
+    }
+    component = <LightbulbIcon sx={{color:"#ffad14"}}/>
+  //For levelling sensors
+  } else if (title.startsWith("L")){
+    if(data<=0){
+      color = "#38ad14"
+    }
+  } else{
+    throw new Error("The bulb type isn't M, L or S")
+  }
   return(
     <>
-      <Box sx={{backgroundColor:on ? '#38ad14' : '#cc1111', borderRadius:1}}>
+      <Box sx={{backgroundColor:'#38ad14'}}>
         <div className="mt-2">
           <span className="mt-1 h4">{title}</span>
-            {component}
+          {component}
+          <div>
+            <Box sx={{backgroundColor:colorPlus}}>
+              <span>+</span>
+            </Box>
+            <Box sx={{backgroundColor:colorMinus}}>
+              <span>-</span>
+            </Box>
+          </div>
         </div>
       </Box>
     </>
--- interface/config/src/pages/Sensor.js
+++ interface/config/src/pages/Sensor.js
@@ -3,34 +3,40 @@ import BinaryBulb from '../components/BinaryBulb';
 
 export default function Sensor(props){
   let [state, setState] = useState({
-    motorX1Connect:false,
-    motorX2Connect:true,
-    motorY1Connect:false,
-    motorZ1Connect:false,
-    motorZ2Connect:false,
-    motorZ3Connect:true,
-    motorZ4Connect:false,
+    //0 : turned off( no signal at all)
+    //1 : turned on( no signal from the sensors )
+    //2 : turned on( signal + from the sensor )
+    //3 : turned on( signal - from the sensor )
+    motorX1Connect:0,
+    motorX2Connect:2,
+    motorY1Connect:3,
+    motorZ1Connect:2,
+    motorZ2Connect:1,
+    motorZ3Connect:3,
+    motorZ4Connect:3,
 
-    Ssensor1X1Connect:false,
-    Ssensor2X1Connect:false,
-    Ssensor1X2Connect:true,
-    Ssensor2X2Connect:false,
-    Ssensor1Y1Connect:false,
-    Ssensor2Y1Connect:false,
+    Ssensor1X1Connect:2,
+    Ssensor2X1Connect:1,
+    Ssensor1X2Connect:3,
+    Ssensor2X2Connect:2,
+    Ssensor1Y1Connect:0,
+    Ssensor2Y1Connect:3,
 
-    Ssensor1Z1Connect:true,
-    Ssensor2Z1Connect:false,
-    Ssensor1Z2Connect:false,
-    Ssensor2Z2Connect:true,
-    Ssensor1Z3Connect:false,
-    Ssensor2Z3Connect:true,
-    Ssensor1Z4Connect:true,
-    Ssensor2Z4Connect:false,
+    Ssensor1Z1Connect:1,
+    Ssensor2Z1Connect:0,
+    Ssensor1Z2Connect:0,
+    Ssensor2Z2Connect:3,
+    Ssensor1Z3Connect:3,
+    Ssensor2Z3Connect:2,
+    Ssensor1Z4Connect:0,
+    Ssensor2Z4Connect:1,
 
-    LsensorZ1Connect:true,
-    LsensorZ2Connect:false,
-    LsensorZ3Connect:false,
-    LsensorZ4Connect:true,
+    //Numbers, measuring distance.
+    //-1 means no connection
+    LsensorZ1Connect:-1,
+    LsensorZ2Connect:-1,
+    LsensorZ3Connect:-1,
+    LsensorZ4Connect:-1,
 
     energySourceConnect:false,
   })
@@ -61,49 +67,49 @@ export default function Sensor(props){
 
             </div>
 
-            <div className="row flex-nowrap justify-content-center">
+            <div className="row flex-nowrap justify-content-center border">
 
               <div className="col-4 p-2">
-                <BinaryBulb on={state['motorX1Connect']} title={"MX 1"} />
-                <BinaryBulb on={state['motorX2Connect']} title={"MX 2"} />
-                <BinaryBulb on={state['motorY1Connect']} title={"MY 1"} />
-                <BinaryBulb on={state['motorZ1Connect']} title={"MZ 1"} />
-                <BinaryBulb on={state['motorZ2Connect']} title={"MZ 2"} />
-                <BinaryBulb on={state['motorZ3Connect']} title={"MZ 3"} />
-                <BinaryBulb on={state['motorZ4Connect']} title={"MZ 4"} />
+                <BinaryBulb data={state['motorX1Connect']} title={"MX 1"} />
+                <BinaryBulb data={state['motorX2Connect']} title={"MX 2"} />
+                <BinaryBulb data={state['motorY1Connect']} title={"MY 1"} />
+                <BinaryBulb data={state['motorZ1Connect']} title={"MZ 1"} />
+                <BinaryBulb data={state['motorZ2Connect']} title={"MZ 2"} />
+                <BinaryBulb data={state['motorZ3Connect']} title={"MZ 3"} />
+                <BinaryBulb data={state['motorZ4Connect']} title={"MZ 4"} />
               </div>
 
               <div className="col-1 p-2">
               </div>
 
               <div className="col-3 p-2">
-                <BinaryBulb on={state['Ssensor1X1Connect']} title={"SSX 1"} />
-                <BinaryBulb on={state['Ssensor2X1Connect']} title={"SSX 2"} />
-                <BinaryBulb on={state['Ssensor1X2Connect']} title={"SSX 3"} />
-                <BinaryBulb on={state['Ssensor2X2Connect']} title={"SSX 4"} />
-                <BinaryBulb on={state['Ssensor1Y1Connect']} title={"SSY 1"} />
-                <BinaryBulb on={state['Ssensor2Y1Connect']} title={"SSY 2"} />
+                <BinaryBulb data={state['Ssensor1X1Connect']} title={"SSX 1"} />
+                <BinaryBulb data={state['Ssensor2X1Connect']} title={"SSX 2"} />
+                <BinaryBulb data={state['Ssensor1X2Connect']} title={"SSX 3"} />
+                <BinaryBulb data={state['Ssensor2X2Connect']} title={"SSX 4"} />
+                <BinaryBulb data={state['Ssensor1Y1Connect']} title={"SSY 1"} />
+                <BinaryBulb data={state['Ssensor2Y1Connect']} title={"SSY 2"} />
               </div>
 
               <div className="col-3 p-2">
-                <BinaryBulb on={state['Ssensor1Z1Connect']} title={"SSZ 1"} />
-                <BinaryBulb on={state['Ssensor2Z1Connect']} title={"SSZ 2"} />
-                <BinaryBulb on={state['Ssensor1Z2Connect']} title={"SSZ 3"} />
-                <BinaryBulb on={state['Ssensor2Z2Connect']} title={"SSZ 4"} />
-                <BinaryBulb on={state['Ssensor1Z3Connect']} title={"SSZ 5"} />
-                <BinaryBulb on={state['Ssensor2Z3Connect']} title={"SSZ 6"} />
-                <BinaryBulb on={state['Ssensor1Z4Connect']} title={"SSZ 7"} />
-                <BinaryBulb on={state['Ssensor2Z4Connect']} title={"SSZ 8"} />
+                <BinaryBulb data={state['Ssensor1Z1Connect']} title={"SSZ 1"} />
+                <BinaryBulb data={state['Ssensor2Z1Connect']} title={"SSZ 2"} />
+                <BinaryBulb data={state['Ssensor1Z2Connect']} title={"SSZ 3"} />
+                <BinaryBulb data={state['Ssensor2Z2Connect']} title={"SSZ 4"} />
+                <BinaryBulb data={state['Ssensor1Z3Connect']} title={"SSZ 5"} />
+                <BinaryBulb data={state['Ssensor2Z3Connect']} title={"SSZ 6"} />
+                <BinaryBulb data={state['Ssensor1Z4Connect']} title={"SSZ 7"} />
+                <BinaryBulb data={state['Ssensor2Z4Connect']} title={"SSZ 8"} />
               </div>
 
               <div className="col-1 p-2">
               </div>
 
               <div className="col-3 p-2">
-                <BinaryBulb on={state['LsensorZ1Connect']} title={"LSZ 1"} />
-                <BinaryBulb on={state['LsensorZ2Connect']} title={"LSZ 2"} />
-                <BinaryBulb on={state['LsensorZ3Connect']} title={"LSZ 3"} />
-                <BinaryBulb on={state['LsensorZ4Connect']} title={"LSZ 4"} />
+                <BinaryBulb data={state['LsensorZ1Connect']} title={"LSZ 1"} />
+                <BinaryBulb data={state['LsensorZ2Connect']} title={"LSZ 2"} />
+                <BinaryBulb data={state['LsensorZ3Connect']} title={"LSZ 3"} />
+                <BinaryBulb data={state['LsensorZ4Connect']} title={"LSZ 4"} />
               </div>
 
             </div>
