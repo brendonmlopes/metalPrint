55e7bb0eb403e327feac0eede74037212c4b88b3
Brendon Maia Lopes <brendonmaial@poli.ufrj.br>
2025-09-01 13:39:27 -0300

    added favicon and adjusted navbar size

--- interface/config/src/components/NavBar.js
+++ interface/config/src/components/NavBar.js
@@ -12,7 +12,7 @@ export default function NavBar(props){
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
       <a className="navbar-brand" onClick={()=>setPage('home')} href="#">
-        <img style={{maxHeight:'80px'}} src={logo}></img>
+        <img style={{maxHeight:'10vh'}} src={logo}></img>
     </a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
